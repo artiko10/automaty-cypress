4261e8e41570657cd5d669e6deb6ca6b72876b1b
artiko10 <artur.gomulka11@gmail.com>
2022-12-27 16:26:48 +0100

    pom checkbox

--- cypress/e2e/checkCheckbox.cy.js
+++ cypress/e2e/checkCheckbox.cy.js
@@ -1,3 +1,5 @@
+import checkboxPage from "./pages/checkboxPage";
+
 describe('check checkbox', () => {
 
     beforeEach(() => {
@@ -6,53 +8,23 @@ describe('check checkbox', () => {
       })
     
       it('check if only one radio button is selected', () => {
-   
-        cy.get('#reverse-radio-1').check()
-        cy.get('#reverse-radio-1').should('be.checked');
-        cy.get('#reverse-radio-2').should('not.be.checked');
-        cy.get('#reverse-radio-2').check()
-        cy.get('#reverse-radio-2').should('be.checked');
-        cy.get('#reverse-radio-1').should('not.be.checked');
-      
+        checkboxPage.BlackCheckBoxIsSelected()
+        checkboxPage.WhiteCheckBoxIsSelected()
       })
 
       it('find which chebox is selected', () => {
-   
-        cy.get('[data-cy="Option1Checkbox"]').invoke("prop", "checked").then(option1 => {
-          cy.log(option1)
-          expect(option1).to.be.true
-
-        })
-        cy.get('[data-cy="Chekboxs task 2"] > :nth-child(2)').invoke("prop", "checked").then(option2 => {
-            cy.log(option2)
-        })
-        
+         checkboxPage.findCheckboxSelected()
       })
 
-      it('Find the bug', () => {
-   
-        cy.get('[data-cy="YesCheckbox"]').check().invoke("prop", "checked").then(yes => {
-          cy.log(yes)
-         })
-         cy.get('[data-cy="NoCheckbox"]').check().invoke("prop", "checked").then(no => {
-          cy.log(no)
-          })
-      
+      it('Find the bug', () => {  
+        checkboxPage.VerifyCheckboxesAreChecked()   
       })
 
       it('Confirm last field is disabled', () => {
-        cy.get('[data-cy="CheckboxOne"]').check()
-        cy.get('[data-cy="CheckboxTwo"]').check()
-        cy.get('[data-cy="CheckboxThree"]').should('be.disabled')
-       
+        checkboxPage.VerifyDisabledCheckbox()  
       })
 
       it('Confirm that all options are selected', () => {
-        cy.get('[data-cy="Chekboxs task 5"]').find("input").check();
-        cy.get('[data-cy="Chekboxs task 5"]').find("input").should('be.checked');
-        cy.get('[data-cy="Chekboxs task 5"] > :nth-child(1) > .form-check-input').uncheck();
-        cy.get('[data-cy="Chekboxs task 5"]').find("input").should('not.be.checked');
-        cy.get('[data-cy="Chekboxs task 5"] > :nth-child(1) > .form-check-input').check();
-        cy.get('[data-cy="Chekboxs task 5"]').find("input").should('be.checked');
+       checkboxPage.VerifyAllOptionSelected()
       })
 })
---
+++ cypress/e2e/pages/checkboxPage.js
@@ -0,0 +1,66 @@
+class checkboxPage {
+    elements ={
+        checboxBlack : () => cy.get('[data-cy="BlackCheckbox"]'),
+        checkboxWhite: () => cy.get('[data-cy="WhiteCheckbox"]'),
+        checkboxOption1: () => cy.get('[data-cy="Option1Checkbox"]'),
+        checkboxsTwoOptions: () => cy.get('[data-cy="Chekboxs task 2"] > :nth-child(2)'),
+        checkboxYes: () => cy.get('[data-cy="YesCheckbox"]'),
+        checkboxNo: () => cy.get('[data-cy="NoCheckbox"]'),
+        checkboxOne: () => cy.get('[data-cy="CheckboxOne"]'),
+        checkboxTwo: () => cy.get('[data-cy="CheckboxTwo"]'),
+        checkboxThree: () => cy.get('[data-cy="CheckboxThree"]'),
+        AllCheckboxTask5: () => cy.get('[data-cy="Chekboxs task 5"]'),
+        SelectAllCheckbox: () => cy.get('[data-cy="Chekboxs task 5"] > :nth-child(1) > .form-check-input')
+    }
+
+    BlackCheckBoxIsSelected() {
+        this.elements.checboxBlack().check()
+        this.elements.checboxBlack().should('be.checked');
+        this.elements.checkboxWhite().should('not.be.checked');
+     
+     }
+
+     WhiteCheckBoxIsSelected() {
+        this.elements.checkboxWhite().check()
+        this.elements.checkboxWhite().should('be.checked');
+        this.elements.checboxBlack().should('not.be.checked');
+     
+     }
+
+     findCheckboxSelected() {
+        this.elements.checkboxOption1().invoke("prop", "checked").then(option1 => {
+            cy.log(option1)
+            expect(option1).to.be.true
+        })
+
+        this.elements.checkboxsTwoOptions().invoke("prop", "checked").then(option2 => {
+            cy.log(option2)
+        })
+     }
+
+     VerifyCheckboxesAreChecked() {
+        this.elements.checkboxYes().check().invoke("prop", "checked").then(yes => {
+            cy.log(yes)
+           })
+           this.elements.checkboxNo().check().invoke("prop", "checked").then(no => {
+            cy.log(no)
+            })
+     }
+
+     VerifyDisabledCheckbox() {
+       this.elements.checkboxOne().check()
+       this.elements.checkboxTwo().check()
+       this.elements.checkboxThree().should('be.disabled')
+     }
+
+     VerifyAllOptionSelected() {
+        this.elements.AllCheckboxTask5().find("input").check();
+        this.elements.AllCheckboxTask5().find("input").should('be.checked');
+        this.elements.SelectAllCheckbox().uncheck();
+        this.elements.AllCheckboxTask5().find("input").should('not.be.checked');
+        this.elements.SelectAllCheckbox().check();
+        this.elements.AllCheckboxTask5().find("input").should('be.checked');
+     }
+
+}
+export default new checkboxPage();
